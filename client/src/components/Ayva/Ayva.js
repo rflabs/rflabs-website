c2260f49d4399c12d3a0e23e51629a797274331f
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-03-03 10:08:26 -0800

    minor ux

--- client/src/components/Ayva/Ayva.js
+++ client/src/components/Ayva/Ayva.js
@@ -14,18 +14,11 @@ class Ayva extends Component {
                             <img className='ayvaArchitectureImg' src={ayvaArchitecture} alt='ayvaArchitecture'/>
                         </div>
                         <div className='ayvaOverview'>
-                            <span><span className='ayva'>Ayva</span> is an open-source cross-platform developer framework used to build voice assistant applications. The features and tools provided in this framework are platform agnostic, allowing developers to build their app once and deploy on both Google Assistant and Alexa.</span>
+                            <span><span className='ayva'>Ayva</span> is an open-source cross-platform developer 
+                            framework used to build voice assistant applications. The features and tools provided in 
+                            this framework are platform agnostic, allowing developers to build their app once and deploy 
+                            on both Google Assistant and Alexa.</span>
                         </div>
-                        <div className='ayvaComponents'>
-                            <h1>The Ayva framework is built of: </h1>
-                        </div>
-                        <div className='ayvaComponentList'>
-                            <ul>
-                                <li>Ayva Library</li>
-                                <li>Ayva CLI</li>
-                                <li>Middleware</li>
-                            </ul>
-                        </div>   
                     </div>
                 </div>
             </div>
