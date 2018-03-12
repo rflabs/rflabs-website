79522669a75ab3875a85c8c007227e0f23ae7fdd
kevin.lee@refreshlabs.co <kevin.lee@refreshlabs.co>
2018-03-11 23:49:36 -0700

    css update

--- client/src/components/Ayva/Ayva.js
+++ client/src/components/Ayva/Ayva.js
@@ -13,12 +13,6 @@ class Ayva extends Component {
                         <div className='ayvaArchitecture'>
                             <img className='ayvaArchitectureImg' src={ayvaArchitecture} alt='ayvaArchitecture'/>
                         </div>
-                        <div className='ayvaOverview'>
-                            <span><span className='ayva'>Ayva</span> is an open-source cross-platform developer 
-                            framework used to build voice assistant applications. The features and tools provided in 
-                            this framework are platform agnostic, allowing developers to build their app once and deploy 
-                            on both Google Assistant and Alexa.</span>
-                        </div>
                     </div>
                 </div>
             </div>
